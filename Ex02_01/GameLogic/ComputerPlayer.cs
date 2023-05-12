49d217bb2589a2ccb4600f4dd04f79e4a3e06c1e
Oz Hayun <ozhayun2@gmail.com>
2023-05-12 10:57:00 +0300

    Delete StupidMove() of computer

--- Ex02_01/GameLogic/ComputerPlayer.cs
+++ Ex02_01/GameLogic/ComputerPlayer.cs
@@ -30,28 +30,6 @@ namespace Ex02_01
                 io_Board.SetRowAndColumnToBeTheFirstClearCell(io_Row, io_Coulmn);
             }
         }
-
-        public void StupidMove(ref Board io_Board, ref int io_Row, ref int io_Column)
-        {
-            GetBlankRandomRowAndCol(io_Board, out io_Row, out io_Column);
-            io_Board.AddPlayerSign(io_Row, io_Column, m_Sign);
-        }
-
-        private void GetBlankRandomRowAndCol(Board i_Board, out int o_Row, out int o_Column)
-        {
-            Random random = new Random();
-            GetRowAndCol(random, i_Board.BoardSize, out o_Row, out o_Column);
-            while (!i_Board.IsThisCellClear(o_Row, o_Column))
-            {
-                GetRowAndCol(random, i_Board.BoardSize, out o_Row, out o_Column);
-            }
-        }
-
-        private void GetRowAndCol(Random i_Random, int i_BoardSize, out int o_Row, out int o_Column)
-        {
-            o_Row = i_Random.Next(1, i_BoardSize);
-            o_Column = i_Random.Next(1, i_BoardSize);
-        }
     }
 }
 
--- Ex02_01/GameLogic/GameWithOnePlayer.cs
+++ Ex02_01/GameLogic/GameWithOnePlayer.cs
@@ -30,7 +30,6 @@ namespace Ex02_01
             {
                 if (m_IsComputerPlayerTurn)
                 {
-                    // m_ComputerPlayer.StupidMove(ref m_Board, ref row, ref column);
                     m_ComputerPlayer.AIMove(ref m_Board, ref row, ref column);
                     currentPlayerSign = m_ComputerPlayer.Sign;
                 }
