ed0c4e5e685c852c3fc259d5baac79e904e2963a
Oz Hayun <ozhayun2@gmail.com>
2023-05-12 15:21:46 +0300

    Add BoardValidator.cs

--- Ex02_01/GameLogic/Board.cs
+++ Ex02_01/GameLogic/Board.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Ex02_01
+﻿namespace Ex02_01
 {
     public class Board
     {
---
+++ Ex02_01/GameLogic/BoardValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Ex02_01
+{
+	public class BoardValidator
+	{
+		Board m_Board;
+
+		public BoardValidator(Board i_Board)
+		{
+			this.m_Board = i_Board;
+		}
+
+
+	}
+}
+
